7f63544c76114de5be7946df5556413f39f930d9
Romeno Wenogk Fernando <romeno@igem.org>
2021-04-16 17:15:33 +0400

    starting shared memory

--- chef.cpp
+++ chef.cpp
@@ -6,12 +6,51 @@
 #include <sys/stat.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+using namespace std;
+
+int tomatoWeight = 80;
+int greenPepperWeight = 50;
+int onionsWeight = 30;
+
+struct Kitchen
+{
+    int cnt;
+    int complete;
+    char buf[20];
+};
 
 int main()
 {
+    int shmid, numtimes;
+    struct Kitchen *kitchenData;
+    char *bufptr;
+    int spaceavailable;
+    shmid = shmget(IPC_PRIVATE, sizeof(struct Kitchen), 0644 | IPC_CREAT);
+    if (shmid == -1)
+    {
+        perror("Shared memory");
+        return 1;
+    }
 
+    // Attach to the segment to get a pointer to it.
+    kitchenData = (Kitchen *)shmat(shmid, NULL, 0);
+
+    if (kitchenData == (void *)-1)
+    {
+        perror("Shared memory attach");
+        return 1;
+    }
+    else
+    {
+        printf("shmid %d\n", shmid);
+    }
+    kitchenData->cnt = 2058;
     // chef
     printf("I am a chef\n");
+    printf("\t tomatoWeight: %d", tomatoWeight);
 
     // 3 SaladMakers, therefore create 3 salad maker processes
     pid_t pids[3];
